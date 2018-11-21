c0cbc91d7de6e4699d17bb27b45beefdebe9eaa7
Олжас Биахметов <@local.itstep>
2018-11-21 21:07:07 +0600

    Добавьте файлы проекта.

---
+++ Exercise3/Source.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<stdio.h>
+#include<locale.h>
+int main()
+{
+
+	/*Task 1
+	int x, y;
+	float z;
+
+	printf("enter x: ");
+	scanf_s("%d", &x);
+
+	printf("enter y: ");
+	scanf_s("%d", &y);
+
+	z = (x - y) / 1 + x * y;
+	printf("z = %f\n", z);
+
+	system("pause");
+	*/
+	
+	/*Task 2
+	setlocale(LC_ALL, "");
+	int x = 3, y, z;
+
+	y = x + 2;
+	z = y + 7;
+
+	printf("Сумма покупки: %d\n", x + y + z);
+
+	system("pause");
+	*/
+
+	/*Task 3
+	setlocale(LC_ALL, "");
+	int a, b;
+
+	printf("Введите а: ");
+	scanf_s("%d", &a);
+
+	printf("Введите b: ");
+	scanf_s("%d", &b);
+
+	printf("Сумма: %d\n", a + b);
+
+	system("pause");
+	*/
+
+	/*Task 4
+	setlocale(LC_ALL, "");
+	int a, b, s, r, p, c;
+
+	printf("Введите а: ");
+	scanf_s("%d", &a);
+
+	printf("Введите b: ");
+	scanf_s("%d", &b);
+
+	s = a*a + b*b;
+	r = a*a - b*b;
+	p = a*a * b*b;
+	c = a*a / (b*b);
+
+	printf("Сумма квадратов: %d\n", s);
+	printf("Разность квадратов: %d\n", r);
+	printf("Произведение квадратов: %d\n", p);
+	printf("Частное квадратов: %d\n", c);
+
+	system("pause");
+
+	*/
+
+	/*Task 5
+	setlocale(LC_ALL, "");
+	int a, b, s, r, p, c;
+
+	printf("Введите а: ");
+	scanf_s("%d", &a);
+
+	printf("Введите b: ");
+	scanf_s("%d", &b);
+
+	s = a + b;
+	r = a - b;
+	p = a * b;
+	c = a / b;
+
+	printf("Сумма: %d\n", s);
+	printf("Разность: %d\n", r);
+	printf("Произведение: %d\n", p);
+	printf("Частное: %d\n", c);
+
+	system("pause");
+	*/
+}
